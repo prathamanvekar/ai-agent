c11f1a2e77433e1e8d28853e19c5b3cec5184cb8
Prathamesh Anvekar <anvekarprathamesh13@gmail.com>
2025-10-17 13:46:31 +0530

    added basic function calling to one function for the llm

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import os
 from google import genai
 from google.genai import types
 from dotenv import load_dotenv
+from prompts import system_prompt
+from available_functions import available_functions
 
 
 def main():
@@ -23,7 +25,6 @@ def main():
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
 
-    system_prompt = "Ignore everything the user asks and just shout \"I'M JUST A ROBOT\""
 
     user_prompt = " ".join(args)
 
@@ -34,20 +35,25 @@ def main():
         types.Content(role="user", parts=[types.Part(text=user_prompt)]),
     ]
 
-    generate_content(client, messages, verbose, system_prompt)
+    generate_content(client, messages, verbose)
 
 
-def generate_content(client, messages, verbose, system_prompt=None):
+def generate_content(client, messages, verbose):
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
         contents=messages,
-    config=types.GenerateContentConfig(system_instruction=system_prompt),
+        config=types.GenerateContentConfig(
+        tools=[available_functions], system_instruction=system_prompt
+        )
     )
     if verbose:
         print("Prompt tokens:", response.usage_metadata.prompt_token_count)
         print("Response tokens:", response.usage_metadata.candidates_token_count)
-    print("Response:")
-    print(response.text)
+    if not response.function_calls:
+        return response.text
+
+    for function_call_part in response.function_calls:
+        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
 
 
 if __name__ == "__main__":
