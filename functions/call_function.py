40520f0ace85f5924d6adc14510bd3230041ab66
Prathamesh Anvekar <anvekarprathamesh13@gmail.com>
2025-10-19 00:40:17 +0530

    it is actually a agent now

--- functions/call_function.py
+++ functions/call_function.py
@@ -4,7 +4,7 @@ from functions.get_files_info import get_files_info
 from functions.get_file_content import get_file_content
 from functions.run_python_file import run_python_file
 from functions.write_file import write_file
-from config import WORKING_DIR
+from functions.config import WORKING_DIR
 
 
 def call_function(function_call_part, verbose=False):
--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from dotenv import load_dotenv
 from prompts import system_prompt
 from available_functions import available_functions
 from functions.call_function import call_function
+from functions.config import MAX_ITERS
 
 
 def main():
@@ -36,7 +37,20 @@ def main():
         types.Content(role="user", parts=[types.Part(text=user_prompt)]),
     ]
 
-    generate_content(client, messages, verbose)
+    iters = 0
+    while True:
+        iters += 1
+        if iters > MAX_ITERS:
+            print(f"Maximum iterations ({MAX_ITERS}) reached.")
+            sys.exit(1)
+        try:
+            final_response = generate_content(client, messages, verbose)
+            if final_response:
+                print("Final response:")
+                print(final_response)
+                break
+        except Exception as e:
+                print(f"Error in generate_content: {e}")
 
 
 def generate_content(client, messages, verbose):
@@ -47,9 +61,16 @@ def generate_content(client, messages, verbose):
         tools=[available_functions], system_instruction=system_prompt
         )
     )
+
     if verbose:
         print("Prompt tokens:", response.usage_metadata.prompt_token_count)
         print("Response tokens:", response.usage_metadata.candidates_token_count)
+
+
+    if response.candidates:
+        for candidate in response.candidates:
+            messages.append(candidate.content)
+
     if not response.function_calls:
         return response.text
 
@@ -64,9 +85,12 @@ def generate_content(client, messages, verbose):
         if verbose:
             print(f"-> {function_call_result.parts[0].function_response.response}")
         function_responses.append(function_call_result.parts[0])
-
     if not function_responses:
         raise Exception("no function responses generated, exiting.")
+    messages.append(types.Content(
+        role="user",
+        parts=function_responses,
+    ))
 
 if __name__ == "__main__":
     main()
